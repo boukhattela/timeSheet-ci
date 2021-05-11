aab92bbd9fc26c273a4a7b8a5d3d567b3f36e5c5
Bassem <bassem.boukhattela@esprit.tn>
2021-05-11 18:03:26 +0200

    initialisation de projet timesheet ci

---
+++ src/main/java/tn/esprit/spring/repository/EntrepriseRepository.java
@@ -0,0 +1,7 @@
+package tn.esprit.spring.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EntrepriseRepository  extends CrudRepository<EntrepriseRepository, Long>{
+
+}
