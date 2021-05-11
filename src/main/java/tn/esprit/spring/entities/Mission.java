ee73e012597d31ce9c1279bce4649420201b7b0b
Bassem <bassem.boukhattela@esprit.tn>
2021-05-11 18:34:01 +0200

    second modification

--- src/main/java/tn/esprit/spring/entities/Mission.java
+++ src/main/java/tn/esprit/spring/entities/Mission.java
@@ -48,10 +48,10 @@ public class Mission implements Serializable {
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getName() {
-		return name;
-	}
+//
+//	public String getName() {
+//		return name;
+//	}
 
 	public void setName(String name) {
 		this.name = name;
