cf145899fdb2e58edd3c1ec6c38f2f05d7484fc4
Bassem <bassem.boukhattela@esprit.tn>
2021-05-19 01:08:04 +0200

    last version

--- src/main/java/tn/esprit/spring/repository/EntrepriseRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EntrepriseRepository  extends CrudRepository<EntrepriseRepository, Long>{
-
-}
